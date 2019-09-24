c6987098b7f7ac6df045f21a9e55f39ac760cf0f
Thomas Ciarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2019-09-24 16:19:27 -0300

    Add files via upload

---
+++ Tp N°2/Entidades/Dulce.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string patente, ConsoleColor color)
+        {
+            base.codigoDeBarras = patente;
+            base.colorPrimarioEmpaque = color;
+            base.marca = marca;
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+            set { this.CantidadCalorias = value; }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine($"Codigo de barra: {base.codigoDeBarras}\nColor: {base.colorPrimarioEmpaque}\nMarca{base.marca}");
+            sb.AppendLine($"CALORIAS : {this.CantidadCalorias}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
