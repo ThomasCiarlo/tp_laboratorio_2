b76b9438bd24e68fc67071b6be0d92e658cc0eaa
alumno <thomasciarlo18@gmail.com>
2019-10-17 21:54:26 -0300

    Empezando tp3

--- Tp Nº3/Clases Abstractas/Persona.cs
+++ Tp Nº3/Clases Abstractas/Persona.cs
@@ -8,5 +8,20 @@ namespace Clases_Abstractas
 {
     public abstract class Persona
     {
+        protected string apellido;
+        protected int dni;
+        protected string nombre;
+        protected ENacionalidad nacionalidad;
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extrajero
+        }
+
+        public string Nombre { get { return this.nombre; } set { this.nombre = value;  } }
+        public string Apellido { get { return this.apellido; } set { this.nombre = apellido; } }
+        public int DNI { get { return 1; } set { this.dni = value; } }
+
     }
 }
